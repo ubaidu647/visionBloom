1c1a248827a4c486bb4c1a31c50a2d3ec16f500d
Ubaid Ullah <ubaidktk052@gmail.com>
2024-07-25 18:22:09 +0500

    updated

--- api/index.js
+++ api/index.js
@@ -10,6 +10,6 @@ app.set('view engine', 'ejs');
 app.get("/", (req,res) => {
     res.render('./index.ejs')
 })
-app.listen(4000, () => {
-    console.log("Server running on  http://localhost:4000")
+app.listen(3000, () => {
+    console.log("Server running on  http://localhost:3000")
 });
